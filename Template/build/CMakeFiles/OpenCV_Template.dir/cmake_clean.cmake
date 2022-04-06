66a7ab5b32b91462f87690ceaae15a9c947efea9
Edoalto-metis <Ergoproxy1>
2022-04-06 12:56:52 +0200

    CMake configuration on template

---
+++ Template/build/CMakeFiles/OpenCV_Template.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenCV_Template.dir/main.cpp.obj"
+  "CMakeFiles/OpenCV_Template.dir/main.cpp.obj.d"
+  "OpenCV_Template.exe"
+  "OpenCV_Template.exe.manifest"
+  "OpenCV_Template.pdb"
+  "libOpenCV_Template.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenCV_Template.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
