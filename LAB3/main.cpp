77c5444131ac6e57c5ed0607a4b8f430a3756009
Edoardo Dalla Zuanna <edoardo.dallazuanna@gmail.com>
2022-04-19 15:48:11 +0200

    Code compiled vith visula studio worked only on that IDE

---
+++ LAB3/main.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+void imageShow(cv::Mat input_image);
+cv::Mat cannyEdgeDetector(cv::Mat input_image);
+cv::Mat hugesTransform(cv::Mat canny_edges);
+
+int main() {
+
+	std::string path = "C:/Users/Edoardo/Pictures/images/";										// insert here images directory
+
+	cv::Mat input_image = cv::imread(path + "road2.png");
+	if (input_image.empty()) {
+		std::printf("Error: Image not found");
+		return EXIT_FAILURE;
+	}
+	imageShow(input_image);
+	// Canny Edge Finder
+	cv::Mat canny_output;
+	canny_output = cannyEdgeDetector(input_image);
+}
+
+void imageShow(cv::Mat input_image) {
+	cv::imshow("Input image", input_image);
+	cv::waitKey(0);
+	cv::destroyAllWindows();
+}
+
+cv::Mat cannyEdgeDetector(cv::Mat input_image) {
+
+	cv::Mat edges;
+	double Th = 600;
+	double Tl = 200;
+	int aperture_size = 3;
+	bool L2_gradient = true;
+	cv::Canny(input_image, edges, Tl, Th, aperture_size, L2_gradient);
+	cv::imshow("Canny algorithm output image", edges);
+	cv::waitKey(0);
+	cv::destroyAllWindows();
+	return edges;
+}
+
+
+cv::Mat hugesTransform(cv::Mat canny_edges) {
+	cv::Mat huges_lines_param;
+	return huges_lines_param;
+}
