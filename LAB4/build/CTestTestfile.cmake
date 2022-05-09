dbc168d116815596db0865ebf94a4fd9b6bc3e63
Edoalto-Metis-VM <edoardo.dallazuanna@studenti.unipd.it>
2022-05-09 17:20:58 +0200

    initial work on lab 4: image loaded and displayed

---
+++ LAB4/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/edoardo/Desktop/Computer-Vision/LAB4
+# Build directory: /home/edoardo/Desktop/Computer-Vision/LAB4/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- LAB4/main.cpp
+++ LAB4/main.cpp
@@ -6,8 +6,8 @@ using namespace cv;
 
 int main(int, char**) {
     std::cout << "Hello, world!\n";
+    string path = "/home/edoardo/Pictures/Computer-Vision/LAB4/LNSRT1/";										// insert here images directory
+    Mat input_image = cv::imread(path + "padova.jpg");
+    imshow("Greta mi fa respirare", input_image);
+    waitKey(0);
 }
-string path = "/Desktop/Computer-Vision/img/LAB4/LNSRT1";										// insert here images directory
-Mat input_image = cv::imread(path + "padova.jpg");
-
-cv::imshow("input image", input_image);
