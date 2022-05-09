74ca454680a4efaa03d57943224fac0892f0bf35
Edoalto-Metis-VM <edoardo.dallazuanna@studenti.unipd.it>
2022-05-09 16:56:11 +0200

    deleted all cmakes files and update gitignore

--- LAB3/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/edoardo/Desktop/Computer-Vision/LAB3
-# Build directory: /home/edoardo/Desktop/Computer-Vision/LAB3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/edoardo/Desktop/Computer-Vision/LAB4
-# Build directory: /home/edoardo/Desktop/Computer-Vision/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
